e9ebd04807cb4cc3778df5cc45c3367ac9e75ed8
Guillaume COSTAGLIOLA <guillaume.costagliola@gmail.com>
2020-04-16 16:40:36 +0200

    new steps to install sqlite

--- sqlite/README.txt
+++ sqlite/README.txt
@@ -4,6 +4,10 @@ INSTALL RASPBIAN SQLITE TEST :
 First on Rapbian, connect to github and git clone this repository in your
 /home/pi path.
 
+$ cd /home/pi
+$ git clone https://github.com/Costa83/db_test_raspbian.git
+$ cd db_test_raspbian/sqlite/
+
 Then follow the install.sh script
 
 Or launch it with
--- sqlite/install.sh
+++ sqlite/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo 'Installing SQLITE tests for Raspbian'
-echo 'Make sure you are in /home/pi path'
+echo 'Make sure you are in /home/pi path and your have done '
+echo '$ git clone https://github.com/Costa83/db_test_raspbian.git'
 #Start the rpi with raspbian pre-installed on it (see RASPBIAN_BUILD install.sh)
 
 echo 'Installation de SQLITE3 sur raspbian'
@@ -9,21 +10,22 @@ sudo apt-get install sqlite3
 sqlite3
 
 echo 'Installing SQLITE3 library'
-git clone https://github.com/sqlite/sqlite.git
-cd sqlite
+git clone https://github.com/LuaDist/libsqlite3
+cd libsqlite3
 ./configure
 make
 ls -l .libs
 sudo su
 make install
 
-echo 'Download des sources de test de sqlite'
-git clone https://github.com/Costa83/db_test_raspbian.git
 chmod +x src/install.sh
-./src/install.sh
+cd src/
+./install.sh
+cd ..
 
 echo 'The executable for the testapp are in /home/pi/sqlite/bin'
-chmod +x /script_test/test_X_rows.sh
-./script_test/test_X_rows.sh
+chmod +x script_test/test_X_rows.sh
+cd script_test/
+./test_X_rows.sh
 
 
